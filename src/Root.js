25d7a0a4b1a6a26b7441f517e41fa97b8f65c107
jandie <eidnaj@live.nl>
2018-06-15 08:15:21 +0200

    Added own async middleware

--- src/Root.js
+++ src/Root.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxPromise from 'redux-promise';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducers from 'reducers';
+import async from './middlewares/async';
 
-export default ({ children, initialState = {} }) => {
-    const store = createStore(reducers, initialState, applyMiddleware(
-        reduxPromise
-    ));
+export default ({children, initialState = {}}) => {
+    const store = createStore(
+        reducers,
+        initialState,
+        applyMiddleware(
+            async
+        ));
 
-    return(
+    return (
         <Provider store={store}>
             {children}
         </Provider>
